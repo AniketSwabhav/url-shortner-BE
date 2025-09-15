9f4b8129a7d04658707a10987468c669041c83dd
AniketSwabhav <Aniket.pardeshi@swabhavtechlabs.com>
2025-09-15 11:14:26 +0530

    xyz

--- model/user/User.go
+++ model/user/User.go
@@ -20,3 +20,5 @@ type User struct {
 	Url          []url.Url                    `json:"url" gorm:"foreignKey:urlID"`
 	Subscription []*subscription.Subscription `json:"Subscription" gorm:"foreignKey:subscriptionID"`
 }
+
+//make a DTO  for this strut for get api
